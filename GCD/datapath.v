9073abbd3c2dd40ffb895621f9022a6820671f6b
HoangViet_01 <dinhhoangviet70@gmail.com>
2026-08-13 23:27:33 +0700

    changes datapath.v and changes trigger.D and changes Register.v and changes tb_Register.v

--- GCD/datapath.v
+++ GCD/datapath.v
@@ -3,83 +3,108 @@ module datapath # (
 ) (
     input [WIDTH - 1:0] X_i,
     input [WIDTH - 1:0] Y_i,
-    input RST, CLK, Start, 
-            y_load, x_load, 
-            GCD_load, x_sel, y_sel,
-    output reg [WIDTH - 1:0] GCD_o,
-    output Done_o, x_neq_y, x_lt_y
-) 
+    input RST, CLK, 
+            y_load, x_load, GCD_load, 
+            x_sel, y_sel,
+    output [WIDTH - 1:0] GCD_o,
+    output x_neq_y, x_lt_y
+);
 
-    reg [WIDTH - 1:0] x, y, Q_x, Q_y, Q_gcd, x_sub_y, y_sub_x;
+    reg [WIDTH - 1:0] Q_x, Q_y, Q_gcd;
+    wire [WIDTH - 1:0] x, y, x_sub_y, y_sub_x;
 
+    // Khối chọn dữ liệu tính toán (Mux).
+    assign x = x_sel ? X_i : x_sub_y;
+    assign y = y_sel ? Y_i : y_sub_x;
+
+    /**
+    Khối trigger lưu trữ giá trị đầu vào và đầu ra trong từng phép tính.
+    **/
+
+    // Trigger D_x.
+    Register # (
+        .WIDTH(WIDTH)
+    ) reg_x (
+        .in(x),
+        .clk(CLK),
+        .rst(RST),
+        .EN(x_load),
+        .out(Q_x)
+    );
+
+    // Trigger D_y.
+    Register # (
+        .WIDTH(WIDTH)
+    ) reg_y (
+        .in(y),
+        .clk(CLK),
+        .rst(RST),
+        .EN(y_load),
+        .out(Q_y)
+    );
+
+    // Trigger D_gcd
+    Register # (
+        .WIDTH(WIDTH)
+    ) reg_gcd (
+        .in(Q_x),
+        .clk(CLK),
+        .rst(RST),
+        .EN(GCD_load),
+        .out(Q_gcd)
+    );
+
+    /**
+    Khối tính toán và so sánh.
+    **/
+
+    // Bộ trừ x_sub_y.
     RBS # (
         .WIDTH(WIDTH)
     ) sub_x (
-        .a(x),
-        .b(y),
+        .a(Q_x),
+        .b(Q_y),
         .out(x_sub_y)
     );
 
+    // Bộ trừ y_sub_x.
     RBS # (
         .WIDTH(WIDTH)
     ) sub_y (
-        .a(y),
-        .b(x),
+        .a(Q_y),
+        .b(Q_x),
         .out(y_sub_x)
     );
 
-    always @ (posedge CLK) begin
-            // RST
-            if (RST == 1'b1) begin
-                Q_x <= (WIDTH - 1)'d0;
-                Q_y <= (WIDTH - 1)'d0;
-                Q_gcd <= (WIDTH - 1)'d0;
-                x <= (WIDTH - 1)'d0;
-                y <= (WIDTH - 1)'d0;
-                x_i <= (WIDTH - 1)'d0;
-                y_i <= (WIDTH - 1)'d0;
-            end else begin
-                Done_o <= 1'b0;
-                if (Start == 1'b1) begin
-                    // Load x
-                    if (x_load == 1'b1) begin
-                        if (x_sel == 1'b1) begin
-                            x <= X_i;
-                        end else begin
-                            x <= x_sub_y;
-                        end
-                    end
-                    
-                    // Load y
-                    if (y_load == 1'b1) begin
-                        if (y_sel == 1'b1) begin
-                            y <= Y_i;
-                        end else begin
-                            y <= y_sub_x;
-                        end
-                    end
-                    
-                // if (x_load == 1'b1) Q_x <= x;
-                // if (y_load == 1'b1) Q_y <= y;
-                // if (GCD_load == 1'b1) Q_gcd <= x;
-            end
+    // 2 bộ so sánh (comp).
+    assign x_neq_y = (Q_x != Q_y);
+    assign x_lt_y = (Q_x < Q_y);
 
-            // Bo so sanh va bo tru
-            if (x != y) begin
-                if (x > y) begin
-                    x_lt_y <= 1'b0;
-                    x_i <= x_sub_y;
-                end else begin
-                    x_lt_y <= 1'b1;
-                    y_i <= y_sub_x;
-                end
-            end else begin
-                if (GCD_load == 1'b1) begin
-                    GCD_o <= Q_x;
-                    Done_o <= 1'b1;
-                end
-            end
-        end
-    end
+    assign GCD_o = Q_gcd;
+
+    // always @ (posedge CLK) begin
+    //     // RST
+    //     if (RST) begin
+    //         Q_x <= (WIDTH - 1)'d0;
+    //         Q_y <= (WIDTH - 1)'d0;
+    //         Q_gcd <= (WIDTH - 1)'d0;
+
+    //         // Khối trigger lưu trữ giá trị đầu vào và đầu ra trong từng phép tính.
+    //         if (x_load) begin 
+    //             Q_x <= x;
+    //         end
+    //         if (y_load) begin
+    //             Q_y <= y;
+    //         end
+
+    //         // Khối tính toán và so sánh.
+    //         if (Q_x != Q_y) begin
+    //             if (Q_x > Q_y) x_i <= x_sub_y;
+    //             else y_i <= y_sub_x;
+    //         end else begin
+    //             if (GCD_load) Q_gcd <= Q_x;
+    //         end
+    //     end
+    // end
 
 endmodule
--- LearnBasics/trigger/trigger_D.v
+++ LearnBasics/trigger/trigger_D.v
@@ -1,18 +1,13 @@
 module trigger_D (
-    input in, clk, pre_rst, rst,
-    output reg out, not_out
+    input in, clk, rst,
+    output reg out
 );
 
-    always @ (posedge clk, negedge rst, negedge pre_rst) begin
+    always @ (posedge clk, posedge rst) begin
         if (!rst) begin
             out <= 1'b0;
-            not_out <= 1'b1;
-        end else if (!pre_rst) begin
-            out <= 1'b1;
-            not_out <= 1'b0;
         end else begin
             out <= in;
-            not_out <= ~in;
         end
     end
     
